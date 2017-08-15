dcc43fa739ca7f54d0946fb40ec5764e3607bc43
Nick Nauert <nicknauert@gmail.com>
2017-08-15 08:55:06 -0400

    adding sites

--- javascript.js
+++ javascript.js
@@ -1,6 +1,100 @@
-var box = document.querySelector(".pics");
-var text = document.querySelector(".copy");
 
-box.addEventListener("MouseOver"). {
 
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let copy1 = document.querySelector("#copy1");
+//
+// let box2 = document.querySelector("#pics2");
+// let copy2 = document.querySelector("#copy2");
+//
+// let box3 = document.querySelector("#pics3");
+// let copy3 = document.querySelector("#copy3");
+//
+// let box4 = document.querySelector("#pics4");
+// let copy4 = document.querySelector("#copy4");
+//
+// let box5 = document.querySelector("#pics5");
+// let copy5 = document.querySelector("#copy5");
+//
+// let box6 = document.querySelector("#pics6");
+// let copy6 = document.querySelector("#copy6");
+//
+// let copy = document.querySelector(".copy");
+//
+// function animStart1(){
+//   copy1.className = "copy-anim";
+// }
+// function animEnd1(){
+//   copy1.className = "copy-anim-re";
+// }
+// function animStart2(){
+//   copy2.className = "copy-anim";
+// }
+// function animEnd2(){
+//   copy2.className = "copy-anim-re";
+// }
+// function animStart3(){
+//   copy3.className = "copy-anim";
+// }
+// function animEnd3(){
+//   copy3.className = "copy-anim-re";
+// }
+// function animStart4(){
+//   copy4.className = "copy-anim";
+// }
+// function animEnd4(){
+//   copy4.className = "copy-anim-re";
+// }
+// function animStart5(){
+//   copy5.className = "copy-anim";
+// }
+// function animEnd5(){
+//   copy5.className = "copy-anim-re";
+// }
+// function animStart6(){
+//   copy6.className = "copy-anim";
+// }
+// function animEnd6(){
+//   copy6.className = "copy-anim-re";
+// }
+//
+// box1.addEventListener("mouseover", animStart1);
+// box1.addEventListener("mouseout", animEnd1);
+//
+// box2.addEventListener("mouseover", animStart2);
+// box2.addEventListener("mouseout", animEnd2);
+// box3.addEventListener("mouseover", animStart3);
+// box3.addEventListener("mouseout", animEnd3);
+// box4.addEventListener("mouseover", animStart4);
+// box4.addEventListener("mouseout", animEnd4);
+// box5.addEventListener("mouseover", animStart5);
+// box5.addEventListener("mouseout", animEnd5);
+// box6.addEventListener("mouseover", animStart6);
+// box6.addEventListener("mouseout", animEnd6);
